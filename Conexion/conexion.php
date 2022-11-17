427b0b635037d8ca8a216d0b2d5dbf3f165eb08e
Alex <103464977+OurAlekey@users.noreply.github.com>
2022-11-17 08:58:56 -0600

    Update conexion.php

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -13,10 +13,10 @@ try {
 //por si no muestra las letras acentuadas 
 //$pdo = new pdo($servidor,$usuario,$clave, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8")));
 try {
-    $usuario = "alex";
-    $passConection = "Alex.2022$";
-    $host = "localhost";
-    $bd = "ticket";
+    $usuario = "hmshxfirmjfxgl";
+    $passConection = "8771a65e0f871f59d78559cb92723d643a022d6b82c5b6f9987b061b2dcf457f";
+    $host = "ec2-3-227-68-43.compute-1.amazonaws.com";
+    $bd = "devq8oscq3asiu";
 
     $conexion = pg_connect(
         "user=" . $usuario . " " .
